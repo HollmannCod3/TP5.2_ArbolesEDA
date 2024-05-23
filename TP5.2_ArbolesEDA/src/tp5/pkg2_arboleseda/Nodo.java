cecb94d74de6ea2ec43af3d3611c2c2bec2c34a7
Nicolas Hollmann <nicolashollmanncod3@gmail.com>
2024-05-23 18:01:37 -0300

    se intenta hacer algo

---
+++ TP5.2_ArbolesEDA/src/tp5/pkg2_arboleseda/Nodo.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp5.pkg2_arboleseda;
+
+/**
+ *
+ * @author Hollmann
+ */
+public class Nodo {
+    
+}
